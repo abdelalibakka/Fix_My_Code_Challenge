1bee49f70f5f9a37bf3f9ca4bdb238059437d3c8
abdelalibakka <pakkatatata@gmail.com>
2023-08-26 07:56:04 -0700

    1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,4 +1,6 @@
+Copy code
 #!/usr/bin/node
+
 /*
     Print a square with the character #
 
@@ -8,16 +10,20 @@
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.stderr.write("Usage: ./print_square.js <size>\n");
+    process.stderr.write("Example: ./print_square.js 8\n");
     process.exit(1);
 }
 
 const size = parseInt(process.argv[2], 10); // Parse the size using base 10 (decimal)
 
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
+}
+
+const row = "#".repeat(size); // Create a row of # characters
+
 for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+    console.log(row); // Print the row 'size' number of times
 }
